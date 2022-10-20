346945764b867784274bdb993819e3d0ad24ecaa
Lakshmi <Lakshmi@NTPLAP-008.chn.nestgroup.net>
2022-10-20 16:47:44 +0530

    push1

---
+++ src/app/searchbook/searchbook.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-searchbook',
+  templateUrl: './searchbook.component.html',
+  styleUrls: ['./searchbook.component.css']
+})
+export class SearchbookComponent implements OnInit {
+
+  constructor(private myapi:ApiService) { }
+
+  bookname=""
+
+
+readValues=()=>{
+  let data={
+    "bookname":this.bookname
+  }
+  console.log(data)
+  this.myapi.searchData(data).subscribe(
+    (response)=>{
+      this.data=response
+    }
+  )
+}
+
+data:any=[]
+
+  ngOnInit(): void {
+  }
+
+}
